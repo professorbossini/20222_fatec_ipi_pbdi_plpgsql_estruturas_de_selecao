25439f82c9431fd3bce062a2ec80493e9aea5d50
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-20 13:00:50 -0300

    apostila09 (estruturas de selecao): testes if e if/else
    bloco de codigo 2.4.5 concluido

--- script.sql
+++ script.sql
@@ -1,9 +1,46 @@
 -- def funcao ():
 -- 	return 1
-CREATE FUNCTION fn_valor_aleatorio_entre(lim_inferior INT, lim_superior INT)
+CREATE OR REPLACE FUNCTION fn_valor_aleatorio_entre(lim_inferior INT, lim_superior INT)
 RETURNS INT AS
 $$
 BEGIN
 	RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
 END;
 $$ LANGUAGE plpgsql;
+----------------------------------
+SELECT fn_valor_aleatorio_entre (2, 10);
+----------------------------------
+
+
+DO $$
+DECLARE
+ valor INT;
+BEGIN
+	SELECT fn_valor_aleatorio_entre(1, 30) INTO valor;
+	RAISE NOTICE 'O valor gerado foi %', valor;
+	IF valor BETWEEN 1 AND 20 THEN
+		RAISE NOTICE 'Metade de % igual a %', valor, valor / 2;
+	END IF;
+END;
+$$
+
+---------------------------------------------
+DO $$
+DECLARE
+	valor INT := fn_valor_aleatorio_entre(1, 100);
+BEGIN
+	IF valor % 2 = 0 THEN
+		RAISE NOTICE 'Valor % é par', valor;	
+	ELSE
+		RAISE NOTICE 'Valor % é ímpar', valor;
+	END IF;
+END;
+$$
+
+------------------------------------------
+
+
+
+
+
+
